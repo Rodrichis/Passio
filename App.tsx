c67e91f9adfce99e016db84bccda395ac7e74098
Rodrichis <rodrigosanches8@gmail.com>
2025-10-05 22:00:31 -0300

    se separa rootstack

--- App.tsx
+++ App.tsx
@@ -4,12 +4,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import DashboardScreen from "./src/screens/Dashboard/DashboardScreen";
+import RegisterClientScreen from "./src/screens/RegisterClientScreen";
+import {RootStackParamList} from "./src/types/navigation";
 
-export type RootStackParamList = {
-  Login: undefined;
-  Register: undefined;
-  Dashboard: undefined;
-};
+
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -20,6 +18,8 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="RegisterClient" component={RegisterClientScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
