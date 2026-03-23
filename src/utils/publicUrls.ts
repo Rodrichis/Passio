6db9dea3732e909b23c6c296cda7371c1c24dacf
Rodrichis <rodrigosanches8@gmail.com>
2026-03-23 12:39:57 -0300

    correcion de ruta app

--- src/utils/publicUrls.ts
+++ src/utils/publicUrls.ts
@@ -1,10 +1,10 @@
-const PUBLIC_APP_BASE_URL = "https://passio.cl/app";
+const PUBLIC_SITE_BASE_URL = "https://passio.cl";
 
 export function getPublicAppBaseUrl() {
-  return PUBLIC_APP_BASE_URL;
+  return PUBLIC_SITE_BASE_URL;
 }
 
 export function buildRegistrationUrl(empresaId?: string | null) {
   if (!empresaId) return "";
-  return `${PUBLIC_APP_BASE_URL}/register/${empresaId}`;
+  return `${PUBLIC_SITE_BASE_URL}/register/${empresaId}`;
 }
